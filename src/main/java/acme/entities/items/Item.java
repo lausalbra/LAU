33254e2b30c22c74f45305e3e6789104d87b50ad
Laura <56022111+lausalbra@users.noreply.github.com>
2022-05-30 13:28:44 +0200

    cleannig

--- src/main/java/acme/entities/items/Item.java
+++ src/main/java/acme/entities/items/Item.java
@@ -3,7 +3,6 @@ package acme.entities.items;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -12,7 +11,6 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.xx1s.Xx1;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
 import acme.roles.Inventor;
@@ -66,6 +64,4 @@ public class Item extends AbstractEntity{
 	@ManyToOne(optional = false)
 	Inventor inventor;
 
-	@OneToOne
-	protected Xx1 xx1;
 }
